4e6fe3177d4267dae5e6383d0dfe0b7e2abc7d5e
katherine <katherinejc103@gmail.com>
2022-09-29 15:51:22 -0400

    commit tree ffbb7d96006d1042fedc87c43c6312d0ce3bc2f7

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
+require_relative '../lib/concerns/memorable'
+
+extend Memorable
+
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -19,13 +23,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
